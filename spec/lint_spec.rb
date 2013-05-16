ba21f888e3508159ffffdb35afbf6a74432c17d1
Mark Billie <mbillie1@gmail.com>
2013-05-16 19:08:59 -0400

    spacing

--- spec/lint_spec.rb
+++ spec/lint_spec.rb
@@ -154,158 +154,158 @@ describe Lint do
 
     context "line_too_long" do
       it "dislikes lines of >= 80 chars" do
-      l.line_too_long_violation?("#{'a' * 80}").should be_true
-    end
-  end
-
-  context "trailing_whitespace" do
-    it "whitespace at the end of the line is a no-no" do
-      l.violation?("def foo(bar) ").should be_true
-    end
-
-    it "a newline is ok by itself" do
-      l.violation?("\n").should be_false
-    end
-  end
-
-  context "and and or" do
-    it "the word 'and' is banned" do
-      l.violation?("foo and bar").should be_true
-    end
-
-    it "the word 'or' is also banned" do
-      l.violation?("1 or nil").should be_true
-    end
-  end
-
-  context "for loops" do
-    it "generally you should not use for loops" do
-      l.violation?("for x in list do").should be_true
-    end
-  end
-
-  context "multiline if then" do
-    it "should not use then in multiline if" do
-      l.violation?("if foo then\nbar\nend").should be_true
-    end
-
-    it "single line if then end is ok" do
-      l.violation?("if true then false end").should be_false
-    end
-  end
-
-  context "multiline strings" do
-    it "ignores multiline strings" do
-      l.strip_multiline_strings("sql = <<-SQL for and or SQL").should == "sql = \"\""
-    end
-
-    it "ignores the other kind too" do
-      l.strip_multiline_strings('"""foo bar baz"""').should == '""'
-    end
-  end
-
-  context "comments" do
-    it "ignores comments" do
-      l.violation?("# a comment").should be_false
-    end
-
-    it "ignores comments even when they otherwise violate the rules" do
-      l.violation?("# def foo bar").should be_false
-    end
-
-    it "does not care what violation the comment breaks" do
-      l.violation?("# and or def foo bar, baz").should be_false
-    end
-
-    it "ignores indented comments" do
-      l.violation?(" # def foo bar").should be_false
-    end
-
-    it "ignores comments indented with soft tabs" do
-      l.violation?("  # def foo bar").should be_false
-    end
-  end
-end
-
-context "#meta_violation?" do
-  it "can optionally check for metaprogramming" do
-    l.meta_violation?("eval").should be_true
-  end
-
-  it "fucking HATES define_method" do
-    l.meta_violation?("define_method").should be_true
-  end
-
-  it "dynamic method invocation via send is bad, mmkay?" do
-    l.meta_violation?('foo.send "foo_#{:bar}"').should be_true
-  end
-
-  it "class eval, why not?" do
-    l.meta_violation?("class_eval &block").should be_true
-  end
-
-  it "module eval is also naughty" do
-    l.meta_violation?("module_eval foo").should be_true
-  end
-
-  it "and even instance eval!" do
-    l.meta_violation?("instance_eval &block").should be_true
-  end
-end
-
-context "#exception_violation?" do
-  it "rescuing Exception is bad" do
-    l.exception_violation?("rescue Exception").should be_true
-  end
-
-  it "rescuing nothing is the same as rescuing Exception" do
-    l.exception_violation?("rescue    ").should be_true
-  end
-
-  it "rescuing an exception that ends with Exception is ok" do
-    l.violation?("rescue MyCustomException").should be_false
-  end
-end
-
-context "string stripping" do
-  it "removes strings so it doesn't break on them" do
-    l.strip_strings("def foo(\"bar\")").should == "def foo(\"\")"
-  end
-
-  it "does the same with single-quoted strings" do
-    l.strip_strings("def foo('bar')").should == "def foo('')"
-  end
-end
-
-context "error messages" do
-  it "keeps track of your mistakes and their lines, defaulting to line #1" do
-    l.violation?("def foo bar")
-    l.errors.first.values.should == [1]
-  end
-
-  it "knows what line number you fucked up on" do
-    l.violation?("def foo bar", 3)
-    l.errors.first.values.should == [3]
-  end
-
-  it "everything has an error message" do
-    expect do
-      Lint::Violations.values.each do |violation|
-        Lint::Messages.fetch(violation)
+        l.line_too_long_violation?("#{'a' * 80}").should be_true
       end
-      Lint::ExceptionViolations.values.each do |exception_violation|
-        Lint::Messages.fetch(exception_violation)
-      end
-      Lint::MetaprogrammingViolations.values.each do |metaprogramming_violation|
-        Lint::Messages.fetch(metaprogramming_violation)
-      end
-    end.to_not raise_error
-  end
-end
+    end
 
-context "#method_missing" do
-  it "implements method_missing intelligently" do
-    expect { l.foo }.to raise_error
+    context "trailing_whitespace" do
+      it "whitespace at the end of the line is a no-no" do
+        l.violation?("def foo(bar) ").should be_true
+      end
+
+      it "a newline is ok by itself" do
+        l.violation?("\n").should be_false
+      end
+    end
+
+    context "and and or" do
+      it "the word 'and' is banned" do
+        l.violation?("foo and bar").should be_true
+      end
+
+      it "the word 'or' is also banned" do
+        l.violation?("1 or nil").should be_true
+      end
+    end
+
+    context "for loops" do
+      it "generally you should not use for loops" do
+        l.violation?("for x in list do").should be_true
+      end
+    end
+
+    context "multiline if then" do
+      it "should not use then in multiline if" do
+        l.violation?("if foo then\nbar\nend").should be_true
+      end
+
+      it "single line if then end is ok" do
+        l.violation?("if true then false end").should be_false
+      end
+    end
+
+    context "multiline strings" do
+      it "ignores multiline strings" do
+        l.strip_multiline_strings("sql = <<-SQL for and or SQL").should == "sql = \"\""
+      end
+
+      it "ignores the other kind too" do
+        l.strip_multiline_strings('"""foo bar baz"""').should == '""'
+      end
+    end
+
+    context "comments" do
+      it "ignores comments" do
+        l.violation?("# a comment").should be_false
+      end
+
+      it "ignores comments even when they otherwise violate the rules" do
+        l.violation?("# def foo bar").should be_false
+      end
+
+      it "does not care what violation the comment breaks" do
+        l.violation?("# and or def foo bar, baz").should be_false
+      end
+
+      it "ignores indented comments" do
+        l.violation?(" # def foo bar").should be_false
+      end
+
+      it "ignores comments indented with soft tabs" do
+        l.violation?("  # def foo bar").should be_false
+      end
+    end
   end
-end
+
+  context "#meta_violation?" do
+    it "can optionally check for metaprogramming" do
+      l.meta_violation?("eval").should be_true
+    end
+
+    it "fucking HATES define_method" do
+      l.meta_violation?("define_method").should be_true
+    end
+
+    it "dynamic method invocation via send is bad, mmkay?" do
+      l.meta_violation?('foo.send "foo_#{:bar}"').should be_true
+    end
+
+    it "class eval, why not?" do
+      l.meta_violation?("class_eval &block").should be_true
+    end
+
+    it "module eval is also naughty" do
+      l.meta_violation?("module_eval foo").should be_true
+    end
+
+    it "and even instance eval!" do
+      l.meta_violation?("instance_eval &block").should be_true
+    end
+  end
+
+  context "#exception_violation?" do
+    it "rescuing Exception is bad" do
+      l.exception_violation?("rescue Exception").should be_true
+    end
+
+    it "rescuing nothing is the same as rescuing Exception" do
+      l.exception_violation?("rescue    ").should be_true
+    end
+
+    it "rescuing an exception that ends with Exception is ok" do
+      l.violation?("rescue MyCustomException").should be_false
+    end
+  end
+
+  context "string stripping" do
+    it "removes strings so it doesn't break on them" do
+      l.strip_strings("def foo(\"bar\")").should == "def foo(\"\")"
+    end
+
+    it "does the same with single-quoted strings" do
+      l.strip_strings("def foo('bar')").should == "def foo('')"
+    end
+  end
+
+  context "error messages" do
+    it "keeps track of your mistakes and their lines, defaulting to line #1" do
+      l.violation?("def foo bar")
+      l.errors.first.values.should == [1]
+    end
+
+    it "knows what line number you fucked up on" do
+      l.violation?("def foo bar", 3)
+      l.errors.first.values.should == [3]
+    end
+
+    it "everything has an error message" do
+      expect do
+        Lint::Violations.values.each do |violation|
+          Lint::Messages.fetch(violation)
+        end
+        Lint::ExceptionViolations.values.each do |exception_violation|
+          Lint::Messages.fetch(exception_violation)
+        end
+        Lint::MetaprogrammingViolations.values.each do |metaprogramming_violation|
+          Lint::Messages.fetch(metaprogramming_violation)
+        end
+      end.to_not raise_error
+    end
+  end
+
+  context "#method_missing" do
+    it "implements method_missing intelligently" do
+      expect { l.foo }.to raise_error
+    end
+  end
 end
