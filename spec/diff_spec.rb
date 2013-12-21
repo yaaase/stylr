1f12e0f9f70a4991dbae8a857364bf9f0fb2b20d
Mark Billie <mbillie1@gmail.com>
2013-12-21 13:24:40 -0500

    wip

---
+++ spec/diff_spec.rb
@@ -0,0 +1,11 @@
+require "stylr"
+
+module Stylr
+  describe Diff do
+    let(:d) { Diff.new }
+
+    it "generates only the lines in a git diff, to avoid checking old code repeatedly" do
+
+    end
+  end
+end
