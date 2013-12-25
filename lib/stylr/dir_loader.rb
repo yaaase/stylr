d149775117f2aaad8ee50cc8a28b9ebd6469592f
Mark Billie <mbillie1@gmail.com>
2013-12-25 14:23:15 -0500

    fixing spacing per stylr standards :)

--- lib/stylr/dir_loader.rb
+++ lib/stylr/dir_loader.rb
@@ -2,8 +2,8 @@ class DirLoader
   attr_reader :filenames
 
   def load_dir(dir)
-    Dir.chdir File.expand_path(dir) do
-      @filenames = Dir.glob('**/*.rb').map{|file| File.expand_path(file)}
+    Dir.chdir(File.expand_path(dir)) do
+      @filenames = Dir.glob('**/*.rb').map { |file| File.expand_path(file) }
     end
     @filenames.reject! do |filename|
       filename =~ /(_spec|_test).rb$/
